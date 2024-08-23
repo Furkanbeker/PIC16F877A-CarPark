7b2c321383943c4cc7a7874f004b58f39d0c93d5
Mustafa Furkan Beker <mustafa.beker@std.medipol.edu.tr>
2024-08-23 14:45:44 +0300

    Files files have been edited

--- project.files/build/default/production/_ext/1178584697/lcd.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/_ext/1178584697/lcd.d  \
- build/default/production/_ext/1178584697/lcd.p1:  \
- ../sonsonson.X/support\ c\ files/lcd.c  \
-../sonsonson.X/support\ c\ files/lcd.h 
--- project.files/build/default/production/newmain.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/production/newmain.d  \
- build/default/production/newmain.p1:  \
- newmain.c  \
-C:/Users/mfb36/Desktop/sonsonson.X/support\ c\ files/lcd.h 
